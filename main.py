512860dc0f83b236e86b2f60b393a84fe4cb4238
BIZONUSERYAKAMO <2860483411@qq.com>
2025-02-09 13:51:59 +0800

    fucklogging and new logger with GBK

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import check
 import threading
 import time
@@ -7,6 +9,20 @@ from win32api import SetConsoleTitle
 
 version = 1
 SetConsoleTitle(f'ATMod Launcher build-{version} | 日志')
+
+class Logger(object):
+    def __init__(self, filename='log.log', stream=sys.stdout):
+        self.terminal = stream
+        self.log = open(filename, 'a')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        self.terminal.flush()  # 不启动缓冲,实时输出
+        self.log.flush()
+
+    def flush(self):
+        pass
 
 def main():
     startTime = time.time()
@@ -29,4 +45,6 @@ def main():
 
 if __name__ == '__main__':
     print(f'ATMod Client build-{version}')
+    sys.stdout = Logger('./dump.log', sys.stdout)
+    sys.stderr = Logger(f'./dump.log', sys.stderr)
     main()
